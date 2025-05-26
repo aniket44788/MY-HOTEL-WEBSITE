651f0841cd17ba256a7b9e9addd90948ef4cd79b
aniket44788 <darkpanda44788@gmail.com>
2025-05-26 19:40:57 +0530

    changecss img slider

--- src/Components/Containers/Content-1/CUSTOM-SLIDER/Customslider.jsx
+++ src/Components/Containers/Content-1/CUSTOM-SLIDER/Customslider.jsx
@@ -1,113 +1,50 @@
-import React, { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
+import first from "../CUSTOM-SLIDER/custom Images/room.jpg";
+import second from "../CUSTOM-SLIDER/custom Images/dining.jpg";
+import third from "../CUSTOM-SLIDER/custom Images/room.jpg";
+import fourth from "../CUSTOM-SLIDER/custom Images/bar.jpg";
+import "./customslider.css";
 
-import "./customslider.css"
+const customslider = () => {
+  const images = [first, second, third, fourth];
+  const [state, setState] = useState(0);
 
-function CustomCarousel({ children }) {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [slideDone, setSlideDone] = useState(true);
-  const [timeID, setTimeID] = useState(null);
-
+  // Autoplay every 5 seconds
   useEffect(() => {
-    if (slideDone) {
-      setSlideDone(false);
-      setTimeID(
-        setTimeout(() => {
-          slideNext();
-          setSlideDone(true);
-        }, 5000)
-      );
-    }
-  }, [slideDone]);
+    const interval = setInterval(() => {
+      setState((prev) => (prev + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
 
-  const slideNext = () => {
-    setActiveIndex((val) => {
-      if (val >= children.length - 1) {
-        return 0;
-      } else {
-        return val + 1;
-      }
-    });
+  const next = () => {
+    setState((state + 1) % images.length);
   };
 
-  const slidePrev = () => {
-    setActiveIndex((val) => {
-      if (val <= 0) {
-        return children.length - 1;
-      } else {
-        return val - 1;
-      }
-    });
-  };
-
-  const AutoPlayStop = () => {
-    if (timeID > 0) {
-      clearTimeout(timeID);
-      setSlideDone(false);
-    }
-  };
-
-  const AutoPlayStart = () => {
-    if (!slideDone) {
-      setSlideDone(true);
-    }
+  const prev = () => {
+    setState((state - 1 + images.length) % images.length);
   };
 
   return (
-    <div
-      className="container__slider"
-      onMouseEnter={AutoPlayStop}
-      onMouseLeave={AutoPlayStart}
-    >
-      {children.map((item, index) => {
-        return (
-          <div
-            className={"slider__item slider__item-active-" + (activeIndex + 1)}
-            key={index}
-          >
-            {item}
+    <div className="slider-container">
+      <div
+        className="slider-track"
+        style={{ transform: `translateX(-${state * 100}%)` }}
+      >
+        {images.map((img, idx) => (
+          <div className="slide" key={idx}>
+            <img src={img} alt={`slide-${idx}`} />
           </div>
-        );
-      })}
-
-      <div className="container__slider__links">
-        {children.map((item, index) => {
-          return (
-            <button
-              key={index}
-              className={
-                activeIndex === index
-                  ? "container__slider__links-small container__slider__links-small-active"
-                  : "container__slider__links-small"
-              }
-              onClick={(e) => {
-                e.preventDefault();
-                setActiveIndex(index);
-              }}
-            ></button>
-          );
-        })}
+        ))}
       </div>
-
-      <button
-        className="slider__btn-next"
-        onClick={(e) => {
-          e.preventDefault();
-          slideNext();
-        }}
-      >
-        {">"}
-      </button>
-      <button
-        className="slider__btn-prev"
-        onClick={(e) => {
-          e.preventDefault();
-          slidePrev();
-        }}
-      >
+      <button className="slider-button left" onClick={prev}>
         {"<"}
+      </button>
+      <button className="slider-button right" onClick={next}>
+        {">"}
       </button>
     </div>
   );
-}
+};
 
-export default CustomCarousel;
+export default customslider;
